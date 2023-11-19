2baa09f072073d5f82f40ce7efb291cdae502671
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-11-19 14:59:29 -0600

    add routes with get requests

--- server/app.py
+++ server/app.py
@@ -20,19 +20,29 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    resp = [response.to_dict() for response in Bakery.query.all()]
+    return make_response(resp, 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    if bakery:
+        resp = bakery.to_dict()
+        status = 200
+    else:
+        resp = {"message": "bakery not found"}
+        status = 404
+    return make_response(resp,status)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    resp = [good.to_dict() for good in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+    return make_response(resp, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return make_response(good.to_dict(), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
